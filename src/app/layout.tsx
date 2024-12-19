ffec95a52b5d8a5487bb95a7a71557e48004074e
PratGit1606 <pratham.hegde2005@gmail.com>
2024-12-19 01:31:01 -0700

    Updated Commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 export const metadata: Metadata = {
   title: "Sherpa - Network with Autonomous Vehicles",
@@ -18,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <ClerkProvider>{children}</ClerkProvider>
+        {children}
       </body>
     </html>
   );
